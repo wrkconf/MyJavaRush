0c616c124f7cda44b3475dec7b7ba88e00554068
wrkconf <wrk.conf@gmail.com>
2017-11-20 21:17:34 +0400

    01

---
+++ src/task08/task0801/Solution.java
@@ -0,0 +1,14 @@
+package task08.task0801;
+
+/* 
+HashSet из растений
+*/
+
+import java.util.HashSet;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        System.out.println("hello");
+        
+    }
+}
