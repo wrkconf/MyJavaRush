a2284c315953b297a564446454803108b0b79fcd
wrkconf <wrk.conf@gmail.com>
2017-12-27 16:54:47 +0400

    27.12.17

--- src/task08/task0813/Solution.java
+++ src/task08/task0813/Solution.java
@@ -1,19 +1,39 @@
 package task08.task0813;
 
 import java.util.HashSet;
+import java.util.Set;
 
 /* 
 20 слов на букву «Л»
 */
-/*
+
 public class Solution {
     public static HashSet<String> createSet() {
-        //напишите тут ваш код
-
+        HashSet<String> set = new HashSet<>();
+        set.add("Лев");
+        set.add("Лев2");
+        set.add("Лев3");
+        set.add("Лев4");
+        set.add("Лев5");
+        set.add("Лев6");
+        set.add("Лев7");
+        set.add("Лев8");
+        set.add("Лев9");
+        set.add("Лев10");
+        set.add("Лев11");
+        set.add("Лев12");
+        set.add("Лев13");
+        set.add("Лев14");
+        set.add("Лев15");
+        set.add("Лев16");
+        set.add("Лев17");
+        set.add("Лев18");
+        set.add("Лев19");
+        set.add("Лев20");
+        return set;
     }
 
     public static void main(String[] args) {
-
+        createSet();
     }
 }
-*/
--- src/task08/task0814/Solution.java
+++ src/task08/task0814/Solution.java
@@ -1,25 +1,28 @@
 package task08.task0814;
 
+import java.io.IOException;
 import java.util.HashSet;
-import java.util.Set;
 
 /* 
 Больше 10? Вы нам не подходите
 */
-/*
-public class Solution {
-    public static HashSet<Integer> createSet() {
-        //напишите тут ваш код
 
+public class Solution {
+    public static HashSet<Integer> createSet() throws IOException {
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i<20;set.add(i++));
+        return set;
     }
 
     public static HashSet<Integer> removeAllNumbersMoreThan10(HashSet<Integer> set) {
-        //напишите тут ваш код
-
+        for (Integer i:set){
+            if(i>10){
+                set.remove(i);
+            }
+        }
+        return set;
     }
 
     public static void main(String[] args) {
-
     }
 }
-*/
