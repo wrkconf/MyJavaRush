dc60adc57e6472a1aff13781118377f3d463ab23
wrkconf <wrk.conf@gmail.com>
2018-01-12 01:31:08 +0400

    12.1.18

--- src/task08/task0815/Solution.java
+++ src/task08/task0815/Solution.java
@@ -1,30 +1,49 @@
 package task08.task0815;
 
 import java.util.HashMap;
-import java.util.HashSet;
+import java.util.Map;
 
 /* 
 Перепись населения
 */
-/*
+
 public class Solution {
     public static HashMap<String, String> createMap() {
-        //напишите тут ваш код
-
+        Map<String, String> map = new HashMap<>();
+        map.put("Егор","Летов");
+        map.put("Сергей","Летов");
+        map.put("Андрей","Лысиков");
+        map.put("Антон","Завбялов");
+        map.put("Джон","Ленон");
+        map.put("Павел","Дуров");
+        map.put("Николай","Дуров");
+        map.put("Илон","Маск");
+        map.put("Владимир","Путин");
+        map.put("Алексей","Навальный");
+        return (HashMap<String, String>) map;
     }
 
     public static int getCountTheSameFirstName(HashMap<String, String> map, String name) {
-        //напишите тут ваш код
-
+        int count=0;
+        for(int i=0;i<10;i++){
+            for (String s : map.keySet()){
+                if (s.equals(name))count++;
+                }
+            }
+        return count;
     }
 
     public static int getCountTheSameLastName(HashMap<String, String> map, String lastName) {
-        //напишите тут ваш код
-
+        int count=0;
+        for(int i=0;i<10;i++){
+            //containsKey
+            for (String s : map.values()){
+                if (s.equals(lastName))count++;
+            }
+        }
+        return count;
     }
 
     public static void main(String[] args) {
-
     }
 }
-*/
--- src/task08/task0816/Solution.java
+++ src/task08/task0816/Solution.java
@@ -1,27 +1,49 @@
 package task08.task0816;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Map;
 
 /* 
 Добрая Зинаида и летние каникулы
 */
-/*
+
 public class Solution {
     public static HashMap<String, Date> createMap() {
-        HashMap<String, Date> map = new HashMap<String, Date>();
+        Map<String, Date> map = new HashMap<String, Date>();
         map.put("Stallone", new Date("JUNE 1 1980"));
+        map.put("Arni", new Date("JANUARY 1 1980"));
+        map.put("Adam", new Date("FEBRUARY 1 1980"));
+        map.put("Tom", new Date("MARCH 1 1980"));
+        map.put("Ilon", new Date("APRIL 1 1980"));
+        map.put("Steave", new Date("MAY 1 1980"));
+        map.put("Bill", new Date("JUNE 1 1980"));
+        map.put("John", new Date("JULY 1 1980"));
+        map.put("Alex", new Date("AUGUST 1 1980"));
+        map.put("Adriana", new Date("SEPTEMBER 1 1980"));
 
-        //напишите тут ваш код
+        return (HashMap<String, Date>) map;
     }
 
     public static void removeAllSummerPeople(HashMap<String, Date> map) {
-        //напишите тут ваш код
 
+        for (Map.Entry<String, Date> entry : map.entrySet()){
+            //System.out.println("ID: " + entry.getKey() + " Дата: " + entry.getValue());
+            SimpleDateFormat dateFormat = new SimpleDateFormat("MM");
+            //System.out.println("месяц: " + dateFormat.format(entry.getValue()));
+            if ((entry.getValue().getMonth()>4)&&(entry.getValue().getMonth()<8)){
+                map.remove(entry.getKey());
+            }
+        }
+
+        for (Map.Entry<String, Date> pair : map.entrySet()){
+            System.out.println("ключ: " + pair.getKey() + " Значение: " + pair.getValue());
+        }
     }
 
     public static void main(String[] args) {
-
+        removeAllSummerPeople(createMap());
     }
 }
-*/
